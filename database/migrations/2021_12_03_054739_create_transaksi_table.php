980f9ffaf226128e48771230c8505365c1ad5fda
selaaulia <selaaulia1307@gmail.com>
2021-12-03 13:16:11 +0700

    Tabel Transaksi

---
+++ database/migrations/2021_12_03_054739_create_transaksi_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->unsignedBigInteger('users_id')->nullable();
+            $table->foreign('id')->references('name')->on('users');
+            $table->date('tanggal_transaksi');
+            $table->integer('jumlah_transaksi');
+            $table->string('bukti_transaksi');
+            $table->string('jenis_transaksi');
+            $table->string('status_transaksi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}

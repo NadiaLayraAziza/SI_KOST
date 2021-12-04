57759cd338907de8a0fb1e918c80796cc1964cf6
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-12-04 16:09:18 +0700

    View Edit Penyewa

--- app/Http/Controllers/PenyewaController.php
+++ app/Http/Controllers/PenyewaController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Penyewa;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PenyewaController extends Controller
@@ -54,9 +56,13 @@ class PenyewaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id_penyewa)
     {
-        //
+        $penyewa = Penyewa::with('users','penyedia','kamar')->find($id_penyewa);
+        $users = User::all();
+        // $penyedia = Penyedia::all();
+        // $kamar = Kamar::all();
+        return view('SuperAdmin.edit', compact('penyewa', 'users', 'penyedia', 'kamar'));
     }
 
     /**
@@ -66,9 +72,25 @@ class PenyewaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id_penyewa)
     {
-        //
+        //melakukan validasi data
+        $request->validate([
+            'id_user' => 'required',
+            'telp_ortu' => 'required',
+            'kost' => 'required',
+            'id_kmr' => 'required',
+            'jangka_waktu' => 'required',
+            'jumlah_waktu' => 'required',
+            'tgl_mulai' => 'required',
+            ]);
+
+        //fungsi eloquent untuk mengupdate data inputan kita
+            Penyewa::find($id_penyewa)->update($request->all());
+
+        //jika data berhasil diupdate, akan kembali ke halaman utama
+            Alert::success('Success', 'Data Penyewa Barang Berhasil Diupdate');
+            return redirect()->route('penyewa.index');
     }
 
     /**
@@ -77,8 +99,10 @@ class PenyewaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id_penyewa)
     {
-        //
+        Penyewa::find($id_penyewa)->delete();
+        Alert::success('Success', 'Data Penyewa berhasil dihapus');
+        return redirect()->route('penyewa.index');
     }
 }

0010c6cbf20ca1fdc814a9cca993260b239c38be
selaaulia <selaaulia1307@gmail.com>
2021-12-05 21:46:26 +0700

    Tabel report

---
+++ database/migrations/2021_12_05_144226_create_report_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id('id_report');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->date('tanggal_report');
+            $table->string('keluhan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}

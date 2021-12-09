58d35952c7c51f126a94342f76c9134969c5a0b4
selaaulia <selaaulia1307@gmail.com>
2021-12-09 23:42:08 +0700

    Create Report

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    protected $table = "report";
+    public $timestamps = false;
+    protected $fillable = [
+        'id_report',
+        'users_id',
+        'tanggal_report',
+        'keluhan'
+    ];
+
+    public function report()
+    {
+        return $this->belongsTo(Report::class);
+    }
 }

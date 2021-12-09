1548fb48ef27e2c4f20b0517f136543084144426
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-12-10 01:24:33 +0700

    Fix Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,10 +46,8 @@
                             @csrf
                             <div class="form-group row">
                                 <label for="nama" class="col-md-4 col-form-label text-md-right">Nama Lengkap</label>
-
                                 <div class="col-md-6">
                                     <input id="nama" type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value="{{ old('nama') }}" required autocomplete="nama" autofocus>
-
                                     @error('nama')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -59,17 +57,14 @@
                             </div>
                             <div class="form-group row">
                                 <label for="alamat" class="col-md-4 col-form-label text-md-right">Alamat (Sesuai KTP)</label>
-
                                 <div class="col-md-6">
                                     <input id="alamat" type="text" class="form-control" name="alamat" required autocomplete="alamat">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -79,7 +74,6 @@
                             </div>
                             <div class="form-group row">
                                 <label for="no_hp" class="col-md-4 col-form-label text-md-right">Nomor HP/WA</label>
-
                                 <div class="col-md-6">
                                     <input id="no_hp" type="text" class="form-control" name="no_hp" required autocomplete="no_hp">
                                 </div>
@@ -93,9 +87,8 @@
                             </div>
                             <div class="form-group row" hidden>
                                 <label for="role" class="col-md-4 col-form-label text-md-right">Role</label>
-
                                 <div class="col-md-6">
-                                    <input id="role" type="text" class="form-control" name="role" required autocomplete="role">
+                                    <input id="role" type="text" class="form-control" name="role" required autocomplete="role" value="penyewa">
                                 </div>
                             </div>
                         </div>

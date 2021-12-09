012a41f7e2fc76ae95dc34d74de350c56abb85dc
Tita Wijayanti <wijayantitita094@gmail.com>
2021-12-09 22:14:35 +0700

    Membuat tampilan payment/pembayaran

---
+++ resources/views/User/payment.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts-user.ViewUser')
+
+@section('content')
+<div class="main-content-wrapper d-flex clearfix">
+    <!-- Mobile Nav (max width 767px)-->
+    <div class="mobile-nav">
+        <!-- Navbar Brand -->
+        <div class="amado-navbar-brand">
+            <a href="index.html"><img src="{{asset('assets-user/img/core-img/logo.png')}}" alt=""></a>
+        </div>
+        <!-- Navbar Toggler -->
+        <div class="amado-navbar-toggler">
+            <span></span><span></span><span></span>
+        </div>
+    </div>
+
+    <!-- Header Area Start -->
+    <header class="header-area clearfix">
+        <!-- Close Icon -->
+        <div class="nav-close">
+            <i class="fa fa-close" aria-hidden="true"></i>
+        </div>
+        <!-- Logo -->
+        <div class="logo">
+            <a href="index.html"><img src="{{asset('assets-user/img/core-img/logo-bejo.png')}}" alt=""></a>
+        </div>
+        <!-- Amado Nav -->
+        <nav class="amado-nav">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="/booking/user">Booking</a></li>
+                <li class="active"><a href="/payment/user">Payment</a></li>
+                <li><a href="cart.html">Report</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container" style="padding-top: 190pt;padding-bottom: 190pt">
+        <div class="row justify-content">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Payment') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group row">
+                                <label for="gambar" class="col-md-4 col-form-label text-md-right">Gambar</label>
+                                <div class="col-md-6">
+                                    {{-- <img class="product" width="200" height="200" @if($user->gambar) src="{{ asset('storage/'.$user->gambar) }}" @endif /> --}}
+                                    <input class="uploads form-control" type="file" style="margin-top: 20px;" name="gambar" ></br>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Tanggal Pembayaran') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="date" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Submit') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

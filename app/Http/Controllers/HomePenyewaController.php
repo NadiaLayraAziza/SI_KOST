38d8d229a7a4a206a4295a7724d05cf9fff4defc
selaaulia <selaaulia1307@gmail.com>
2021-12-27 00:54:02 +0700

    buat method pilih kamarhome penyewa controlller

--- app/Http/Controllers/HomePenyewaController.php
+++ app/Http/Controllers/HomePenyewaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kamar;
 use App\Models\Penyedia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomePenyewaController extends Controller
 {
@@ -18,5 +20,14 @@ class HomePenyewaController extends Controller
     {
         $penyedia = Penyedia::with('users')->find($id_penyedia);
         return view('User.detail', compact('penyedia'));
+    }
+    public function PilihKamar($id_penyedia)
+    {
+        //$penyedia = Penyedia::with('users')->where('id_penyedia', $id_penyedia)->first();
+        //$kamar = Kamar::all();
+        //$penyedia = Penyedia::where('id_user', Auth::user()->id)->value('id_penyedia');
+        $kamar = Kamar::with('penyedia')->where('id_penyedia', $id_penyedia)->get();
+        //$penyedia = Penyedia::with('users')->find($id);
+        return view('User.kamar', compact('kamar'));
     }
 }
